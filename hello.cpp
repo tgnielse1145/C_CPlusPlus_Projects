6564125305d1c965d13e1e4f295de3e2661e6771
Todd Nielsen <tgnielse1145@gmail.com>
2023-01-14 21:58:36 -0700

    helloworld

---
+++ hello.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main(){
+    cout << "Hello World " << endl;
+    return 0;
+}
+//git config --global user.email "tgnielse1145@gmail.com"
+  //git config --global user.name "Todd Nielsen"
